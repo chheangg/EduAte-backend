29c6ef58ab747f7f400229a44697d848cdb516a4
chheangg <balldremz@gmail.com>
2024-05-25 04:38:03 -0700

    Implement post request for category

--- src/features/category/category.controller.ts
+++ src/features/category/category.controller.ts
@@ -1,12 +1,19 @@
 import { CategoryService } from './category.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCategoryDto } from './create-category.dto';
 
-@Controller('categories')
+@Controller('/categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Get()
   findAllCategories() {
     return this.categoryService.findAllCategories();
+  }
+
+  @Post()
+  createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    console.log(createCategoryDto);
+    return this.categoryService.createCategory(createCategoryDto);
   }
 }
--- src/features/category/category.service.ts
+++ src/features/category/category.service.ts
@@ -2,6 +2,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './category.entity';
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './create-category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -12,5 +13,19 @@ export class CategoryService {
 
   findAllCategories(): Promise<Category[]> {
     return this.categoryRepository.find();
+  }
+
+  createCategory(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const isCategoryExist = this.categoryRepository.findOneBy({
+      name: this.createCategory.name,
+    });
+
+    if (isCategoryExist) {
+      return null;
+    }
+
+    const category: Category = new Category();
+    category.name = createCategoryDto.name;
+    return this.categoryRepository.save(category);
   }
 }
